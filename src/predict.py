d0441ddf2bdc619f3b0de99b93252a47446d1ac6
tatigabru <travellingtati@gmail.com>
2020-05-17 01:07:51 +0100

    Use new predict

--- src/predict.py
+++ src/predict.py
@@ -1,136 +1,269 @@
+"""
+Generate model preditions for the test data
+"""
+import argparse
+import collections
 import os
-import scipy
-import albumentations as A
-import cv2
+import pickle
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from pytorch_toolbelt.utils import fs
-from pytorch_toolbelt.utils.torch_utils import tensor_from_rgb_image, to_numpy
+from tqdm import tqdm
+
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
 import torch.utils.data
-from torch import optim
 from torch.utils.data import DataLoader
-import segmentation_models_pytorch as smp
+from torch import Tensor
 
-from utils.utils import set_seed, load_model 
-from datasets.dataset_ade20k import ADE20KDataset
-from models.get_models import get_unet, get_fpn
-from configs import TRAIN_IMAGES, TRAIN_MASKS, NUM_CLASSES, IMG_SIZE, VALID_IMAGES, VALID_MASKS      
+from .configs import *
+# current project imports
+from .datasets.spacenet import SARDataset
+from .datasets.spacenet_rgb import RGBADataset
+from .datasets.transforms import TRANSFORMS
+from .models.get_models import get_unet
+from .utils.utils import load_model, load_model_optim, set_seed, write_event
 
 
-def plot_predictions(input_image, prediction, predictions_dir: str, checkpoint_name: str, n_images: int = 2, apply_softmax: bool = True):
+def generate_validation_preds(model: nn.Module, predictions_dir: str, checkpoint: str, debug: bool, fold: int=0, save_oof: bool=True, img_size: int=IMG_SIZE, 
+                        num_workers: int = 2, batch_size: int = 2):
     """
-    Takes as input 3 PyTorch tensors, plots the input image, predictions and targets
+    Load model weights from the checkpoint, 
+    Makes validation predictions and saves oof as images
+    
+        Args: 
+            predictions_dir: directory for saveing predictions
+            checkpoints_dir: directory with weights 
+            model_name     : string name from the models configs listed in models.py file
+            fold           : evaluation fold number, 0-3
+            debug          : if True, runs debugging on few images
+            img_size       : size of images for validation
+            batch_size     : number of images in batch
+            num_workers    : number of workers available
+                   
+    """
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("device: ", device)    
+    os.makedirs(predictions_dir, exist_ok=True)
+
+    df = pd.read_csv(f'{TRAIN_DIR}folds.csv')
+    df_val = df[df.fold == fold]
+    # dataset for validation
+    valid_dataset = SARDataset(
+                sars_dir  = TRAIN_SAR, 
+                masks_dir = TRAIN_MASKS,
+                labels_df = df_val, 
+                img_size  = img_size,                
+                transforms= TRANSFORMS["hflip"],
+                preprocess= True,
+                normalise = True,              
+                debug     = debug,   
+    )     
+    # dataloaders for train and validation    
+    dataloader_valid = DataLoader(valid_dataset,
+                                  num_workers=num_workers,
+                                  batch_size=batch_size,
+                                  shuffle=False,
+                                  drop_last=True)
+    print('{} validation images'.format(len(valid_dataset)))  
+
+    # load model checkpoint       
+    try:
+        print("load", checkpoint)
+        model = load_model(model, checkpoint)
+    except FileNotFoundError:
+        break
+    model = model.to(device)
+    model.eval()
+
+    progress_bar = tqdm(dataloader_valid, total=len(dataloader_valid))        
+    for batch_num, (img, target, tile_ids) in enumerate(progress_bar):  # iterate over batches
+        img = img.to(device)
+        target = target.float().to(device)
+        output = model(img)          
+        # save oof for some batches
+        if batch_num and batch_num == 0:
+            output = torch.sigmoid(output) 
+            output = output.cpu().numpy().copy()
+            for num, pred in enumerate(output, start=0):
+                tile_name = tile_ids[num]
+                if pred.ndim == 3:
+                        pred = np.squeeze(pred, axis=0)
+                prob_mask = np.rint(pred*255).astype(np.uint8) 
+                prob_mask_rgb = np.repeat(prob_mask[...,None], 3, 2) # repeat array for three channels    
+                if debug: print(f"{predictions_dir}/{tile_name}.png")
+                cv2.imwrite(f"{predictions_dir}/{tile_name}.png", prob_mask_rgb)    
+
+
+def generate_test_preds(model: nn.Module, debug: bool, predictions_dir: str, save_oof: bool=True, img_size: int=IMG_SIZE, num_workers: int=2, batch_size: int=8):
+    """
+    Make test predicitons
     
     Args: 
-        input_image: the input image
-        predictions: the predictions thresholded at 0.5 probability        
+        model_name : string name from the models configs listed in models.py file
+        fold: evaluation fold number, 0-3
+        debug: if True, runs the debugging on few images 
+        epochs: number of epochs to train
+        checkpoints_dir: directory with weights 
+        from_epoch, to_epoch: the first ad last epochs for predicitions generation  
     """
-    # Only select the first n images
-    prediction = prediction[:n_images]
+    # creates directories for predicitons
+    predictions_dir = f"{preds_dir}/test_oof/"
+    os.makedirs(predictions_dir, exist_ok=True)
+    print("\n", model_name, "\n")
+    
+    # test dataset
+    test_dataset = TestSARDataset(
+                sars_dir  = TEST_SAR,          
+                img_size  = img_size,                
+                transforms= TRANSFORMS["hflip"],
+                preprocess= True,
+                normalise = True,              
+                debug     = debug,   
+    )       
+
+    # dataloaders for train and validation    
+    dataloader_test = DataLoader(test_dataset,
+                                 num_workers=num_workers,
+                                 batch_size=batch_size,
+                                 shuffle=False,
+                                 drop_last=False)
+    print("{} test images".format(len(dataset_test)))
+
+    # load model checkpoint
+    checkpoint = f"{checkpoints_dir}/{model_name}/{*}_best_val_loss.pt"
+    print("load", checkpoint)
+    try:
+        model = load_model(model, checkpoint)
+    except FileNotFoundError:
+        break
+
+    progress_bar = tqdm(dataloader_test, total=len(dataloader_test))          
+        for batch_num, (img, target, tile_ids) in enumerate(progress_bar):  # iterate over batches
+            img = img.to(device)
+            target = target.float().to(device)
+            output = model(img)  
+            output = torch.sigmoid(output) 
+            output = output.cpu().numpy().copy()
+            for num, pred in enumerate(output, start=0):
+                tile_name = tile_ids[num]
+                if pred.ndim == 3:
+                    pred = np.squeeze(pred, axis=0)
+                prob_mask = np.rint(pred*255).astype(np.uint8) 
+                prob_mask_rgb = np.repeat(prob_mask[...,None], 3, 2) # repeat array for three channels    
+                if debug: print(f"{predictions_dir}/{tile_name}.png")
+                cv2.imwrite(f"{predictions_dir}/{tile_name}.png", prob_mask_rgb)  
+         
+
+def plot_preds(input_image: np.array, prediction: np.array, target: Tensor, predictions_dir: str, n_images: int = 2, save_fig: bool = True) -> None:
+    """
+    Takes as input PyTorch tensors, plots the input image, predictions and targets
+    
+    Args: 
+        input_image    : the input image
+        predictions    : the predictions thresholded at 0.5 probability      
+        target         : Tensor of the targets
+        predictions_dir: directory with oof predictions
+        n_images       : number of images to tack on the plot
+        save_fig       : if true, saves the figure. Default: True
+
+    """
+    # Only select the first n images in a batch
+    prediction = prediction[:n_images]    
+    # binarise prediction
+    prediction = torch.sigmoid(prediction)
+    prediction.round()
     target = target[:n_images]
     input_image = input_image[:n_images]
 
     prediction = prediction.detach().cpu().numpy()
-    if apply_softmax:
-        prediction = scipy.special.softmax(prediction, axis=1)
-
     preds = np.hstack(prediction)
     preds_rgb = np.repeat(preds[..., None], 3, axis=2) # repeat for 3 channels to plot
-    thresholded_pred = np.repeat(preds_rgb[..., None] > 0.5, 3, axis=2)
-    input_im = np.hstack(input_image.cpu().numpy().transpose(0,2,3,1))     
+    #thresholded_pred = np.repeat(preds_rgb[..., None] > 0.5, 3, axis=2)
     
-    if input_im.shape[2] == 3:
-        input_im_grayscale = np.repeat(input_im.mean(axis=2)[..., None], 3, axis=2)
-        overlayed_im = (input_im_grayscale*0.6 + preds_rgb*0.7).clip(0,1)
-    else:
-        input_map = input_im[...,3:]
-        overlayed_im = (input_map*0.6 + preds_rgb*0.7).clip(0,1)    
+    input_image = input_image.cpu().numpy().transpose(0,2,3,1)
+    input_im = np.hstack(input_image[2])  # use Exy channel only for visualisation
+    input_rgb = np.repeat(input_im[..., None], 3, axis=2) # repeat channel 3 times to plot
+    overlayed_im = (input_rgb*0.6 + preds_rgb*0.7).clip(0,1)   
+
+    target = target.detach().cpu().numpy()
+    target = np.hstack(target)
+    target_rgb = np.repeat(target[..., None], 3, axis=2) # repeat for 3 channels to plot 
 
     fig = plt.figure(figsize=(12,26))
-    plot_im = np.vstack([input_im[...,:3], overlayed_im, preds_rgb, target_rgb]).clip(0,1).astype(np.float32)
+    plot_im = np.vstack([input_rgb, overlayed_im, preds_rgb, target_rgb]).clip(0,1).astype(np.float32)
     plt.imshow(plot_im)
     plt.axis("off")
-    plt.savefig(os.path.join(predictions_dir,"preds_{}_epoch_{}.png".format(model_name, epoch))) 
+    if save_fig:
+        plt.savefig(os.path.join(predictions_dir, f"preds_{checkpoint_name}.png")) 
+    plt.show()
 
-
-def run_predict(model, test_folder: str, checkpoint_name: str, predictions_dir: str, debug: bool = False, img_size: int = IMG_SIZE, 
-                batch_size: int = 8, num_workers: int = 4):
+def plot_oof(input_image: Tensor, prediction: Tensor, target: Tensor, predictions_dir: str, n_images: int = 2, save_fig: bool = True) -> None:
     """
-    Run predicitons of the model checkpoint
+    Takes as input PyTorch tensors, plots the input image, predictions and targets
     
     Args: 
-        model : PyTorch model
-        test_folder: directory with test images        
-        checkpoint_name: name of the checkpoint
-        predictions_dir: directory for saving predictions
-        debug: if True, runs the debugging on few images 
-        img_size: size of images for training (for pregressive learning)
-        batch_size: number of images in batch
-        num_workers: number of workers available   
-        plot_preds: boolean flag, if plot oof predictions and save them as png 
-        save_oof: boolean flag, if calculate oof predictions and save them in pickle         
-    Output:
-        oof: test predicitons         
-    """ 
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(device)
-    model = model.to(device)       
-    print(f'\n {checkpoint_name} \n')
+        input_image    : the input image
+        predictions    : the predictions thresholded at 0.5 probability      
+        target         : Tensor of the targets
+        predictions_dir: directory with oof predictions
+        n_images       : number of images to tack on the plot
+        save_fig       : if true, saves the figure. Default: True
+
+    """
+    # Only select the first n images in a batch
+    prediction = prediction[:n_images]    
+    # binarise prediction
+    prediction = torch.sigmoid(prediction)
+    prediction.round()
+    target = target[:n_images]
+    input_image = input_image[:n_images]
+
+    prediction = prediction.detach().cpu().numpy()
+    preds = np.hstack(prediction)
+    preds_rgb = np.repeat(preds[..., None], 3, axis=2) # repeat for 3 channels to plot
+    #thresholded_pred = np.repeat(preds_rgb[..., None] > 0.5, 3, axis=2)
     
-    # test dataset
-    test_dataset = ADE20KTestDataset(
-                images_dir = test_folder, 
-                img_size = img_size, 
-                transforms = None,           
-                debug = debug,)                           
+    input_image = input_image.cpu().numpy().transpose(0,2,3,1)
+    input_im = np.hstack(input_image[2])  # use Exy channel only for visualisation
+    input_rgb = np.repeat(input_im[..., None], 3, axis=2) # repeat channel 3 times to plot
+    overlayed_im = (input_rgb*0.6 + preds_rgb*0.7).clip(0,1)   
 
-    dataloader_test = DataLoader(test_dataset,
-                                  num_workers=num_workers,
-                                  batch_size=batch_size,
-                                  shuffle=False)
-    print('{} test images'.format(len(test_dataset)))
+    target = target.detach().cpu().numpy()
+    target = np.hstack(target)
+    target_rgb = np.repeat(target[..., None], 3, axis=2) # repeat for 3 channels to plot 
 
-    with torch.no_grad():
-        model.eval()        
-        progress_bar = tqdm(dataloader_test, total=len(dataloader_test))
-        oof = collections.defaultdict(list)
-
-        for iter_num, (img, sample_ids) in enumerate(progress_bar):
-            img = img.to(device)  
-            prediction = model(img)             
-            
-            prediction = F.softmax(prediction, dim=1)             
-            # Visualise the first prediction
-            if iter_num == 0 and plot_preds:
-                plot_predictions(img, prediction, predictions_dir, checkpoint_name)    
-            oof["preds"].append(prediction.cpu().numpy().copy())              
-    if save_oof:  
-        pickle.dump(oof, open(f"{predictions_dir}/{checkpoint_name}.pkl", "wb"))    
-    return oof
-    
+    fig = plt.figure(figsize=(12,26))
+    plot_im = np.vstack([input_rgb, overlayed_im, preds_rgb, target_rgb]).clip(0,1).astype(np.float32)
+    plt.imshow(plot_im)
+    plt.axis("off")
+    if save_fig:
+        plt.savefig(os.path.join(predictions_dir, f"preds_{checkpoint_name}.png")) 
+    plt.show()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    arg = parser.add_argument    
-    arg('--predictions-dir', type=str, default='../../../oof', help='Directory for saving oof preds')
-    arg('--results-dir', type=str, default='../../../results/runs', help='Directory for saving checkpoints')
-    arg('--test-dir', type=str, default='../../../data/test', help='Directory for test images')
-    arg('--image-size', type=int, default=224, help='Image size for training')
-    arg('--batch-size', type=int, default=4, help='Batch size during training')
-    arg('--num-workers', type=int, default=4, help='Number of workers for dataloader. Default = 4.')
-    arg('--epochs', type=int, default=10, help='Epoch to run')    
-    arg('--checkpoint', type=str, default=None, help='Checkpoint filename with initial model weights')
-    arg('--debug', type=bool, default=True)
-    args = parser.parse_args() 
-                
-    set_seed(seed=1234)
+    arg = parser.add_argument
+    arg("--model", type=str, default="se_resnext101_dr0.75_512", help="String model name from models dictionary")
+    arg("--seed", type=int, default=1234, help="Random seed")
+    arg("--fold", type=int, default=0, help="Validation fold")
+    arg("--checkpoints_dir", type=str, default="../../checkpoints", help="Directory for loading model weights")
+    arg("--from-epoch", type=int, default=1, help="Resume training from epoch")
+    arg("--to-epochs", type=int, default=15, help="Number of epochs to run")
+    arg("--debug", type=bool, default=False, help="If the debugging mode")
+    args = parser.parse_args()
+
+    set_seed(args.seed)
     
-    checkpoint_file = os.path.join(args.results_dir, args.checkpoint) 
-    # load checkpoint
-    checkpoint = load_model(checkpoint_file)
-    checkpoint_epoch = checkpoint['epoch']
-   
+    weights = f"{args.checkpoints_dir}/{args.model}_fold_{args.fold}/"
+
+    predict_test(
+        model_name=args.model, fold=args.fold, debug=args.debug, checkpoints_dir=weights, save_oof=True, img_size=IMG_SIZE, from_epoch=0, to_epoch=10
+    )
+
+
+if __name__ == "__main__":
+    main()
